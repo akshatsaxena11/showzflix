bd28ac72669bb83951790e62dcf9303e77f95d52
akshatsaxena11 <69255646+akshatsaxena11@users.noreply.github.com>
2022-07-23 15:29:29 +0530

    Update Search.jsx

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -21,7 +21,7 @@ const Search  = () => {
  const [movies,setMovies] = useState([]);
  const [searchValue,setSearchValue] = useState('');
 
- const getMovieRequest = (searchValue) =>{
+ const getMovieRequest = async (searchValue) =>{
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
     const response = await fetch(url);
